ae98440ba1a6e614f2a734def97b2503f7753367
Tedysu0916 <1604032249@qq.com>
2025-05-14 08:08:53 +0800

    update metrics

--- run/inference.sh
+++ run/inference.sh
@@ -18,3 +18,8 @@ python run_vton.py --model_path examples/model/model_1.png --cloth_path examples
 python run_ip_vton.py --model_path examples/model/model_1.png --cloth_path examples/garment/03032_00.jpg --model_type hd --category 0 --scale 2.0 --sample 4
 
 python run_vton_768.py --model_path examples/model/model_1.png --cloth_path examples/garment/03032_00.jpg --model_type hd --category 0 --scale 2.0 --sample 4
+
+
+python inference.py --model_type hd --category 0 --scale 2.0 --sample 4 --gpu_id 4
+
+scale指的guidance scale
